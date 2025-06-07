40bc5ccd96c1e85268566ea5db7e07405a9d0cf7
Adishree Das <89429561+dasadishree@users.noreply.github.com>
2025-06-06 20:33:03 -0400

    Delete Desktop directory im so scared this is gonna mess smth up

--- Desktop/codingprojects/mysite-main/script.js
+++
@@ -1,7 +0,0 @@
-// typing function
-var typed = new Typed(".auto-type", {
-    strings: ["a highschooler from NY", "a woman in STEM", "a technology enthusiast", "an aspiring developer", "a hackathon organizer"],
-    typeSpeed: 200,
-    backSpeed: 200,
-    loop: true
-})
